1ea4926dffdae1c816c0c13be17b62e1e5e2e618
liueric101 <liu493@usc.edu>
2017-10-16 15:48:13 -0700

    Add files via upload

---
+++ NANDgate.ino
@@ -0,0 +1,41 @@
+#define A 3
+#define B 4
+#define Q 2
+
+void setup() {
+  pinMode(A, OUTPUT);
+  pinMode(B, OUTPUT);
+  pinMode(Q, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int nand;
+  for( int i =0; i < 4; i++)
+  {
+    if( i%2 == 0)
+    {
+      digitalWrite(B, LOW);
+    }
+    else
+    {
+      digitalWrite(B, HIGH);
+    }
+    if(i>=2)
+    {
+      digitalWrite(A, HIGH);
+    }
+    else
+    {
+      digitalWrite(A, LOW);
+    }
+    nand = digitalRead(Q);
+    Serial.write("A = "); 
+    Serial.write(A);
+    Serial.write(", B = "); 
+    Serial.write(B);
+    Serial.write(", Q = "); 
+    Serial.write(Q);
+  }
+
+}
